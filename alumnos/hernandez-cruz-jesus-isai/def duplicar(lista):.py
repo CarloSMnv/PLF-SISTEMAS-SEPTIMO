61cd8c441e35c6d233fbd096635b62eea36591b5
isaixpe <cisai9071@gmail.com>
2026-09-22 16:22:11 -0600

    Add files via upload
    actividad 4 duplicar lista

---
+++ alumnos/hernandez-cruz-jesus-isai/def duplicar(lista):.py
@@ -0,0 +1,13 @@
+def duplicar(lista):
+
+    resultado = []
+
+    for x in lista:
+
+        resultado.append(x * 2)
+
+    return resultado
+
+duplicar([1, 2, 3, 4])   # [2, 4, 6, 8]
+
+print(duplicar([1, 2, 3, 4]))
