77378bbb48b85cee16b9037be00a86c32b399709
isaixpe <cisai9071@gmail.com>
2026-09-24 13:07:07 -0600

    Refactor DUPLICAR.py to include main function
    Added main function to call duplicar and print results.

--- alumnos/hernandez-cruz-jesus-isai/DUPLICAR.py
+++ alumnos/hernandez-cruz-jesus-isai/DUPLICAR.py
@@ -1,13 +1,15 @@
 def duplicar(lista):
-
     resultado = []
 
     for x in lista:
-
         resultado.append(x * 2)
 
     return resultado
 
-duplicar([1, 2, 3, 4])   # [2, 4, 6, 8]
 
-print(duplicar([1, 2, 3, 4]))
+def main():
+    print(duplicar([1, 2, 3, 4]))
+
+
+if __name__ == "__main__":
+    main()
